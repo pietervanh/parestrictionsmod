c96b8a651ff4c549bc0662144ba5c858df502d04
upgraz <tripax@gmail.com>
2014-01-11 16:45:15 +0100

    restrictions test but not good enough hotbuild still works

--- ui/mods/restrictions/live_game/restrictions_live.js
+++ ui/mods/restrictions/live_game/restrictions_live.js
@@ -7,31 +7,29 @@ var restrictions = ["/pa/units/air/air_factory/air_factory.json","/pa/units/land
 
 function checkRestrictions(unitid)
 {
-  var result = true;
-  for(var restriction in restrictions)
-  {
-      if(restriction == unitid)
-      {
-         result = false;
-         break;  
-      }
-  }
-  return result;
-}
+	var result = true;
+	_.forEach(restrictions, function(restriction)
+	{
+	//debugger;
+	  if(restriction == unitid)
+	  {
+		
+		 result = false;
+		 return false;  
+	  }
+	});
+	return result;
+};
 
-//if (settings.applyrestrictions === "ON") {
-
-
-    //replace buildbar item
-    //prio must be higher than hotbuild 2 otherwise it keypreview will fail
-    $('.div_build_item').replaceWith(
-                    '<div class="div_build_item" id="bld1" data-bind="visible: checkRestrictions($data.id()), event: { mouseover: function () { $parent.setBuildHover($index()) },' +
-                    '                                                          mousedown: function (data,event) { $parent.executeStartBuild(event, $index())} }">' +
-                    '    <span class="span_build_count" data-bind="text: count, visible: count() > 0"></span>' +
-                    '    <a href="#" data-bind="rollover_sound_exclusive: { sound: 'default', group: $index()}">' +
-                    '        <img class="img_build_unit" src="img/build_bar/units/build_unit_sample.png" data-bind="attr: { src: icon }" />' +
-                    '    </a>'+
-                    '</div>'
+//replace buildbar item
+//prio must be higher than hotbuild 2 otherwise it keypreview will fail
+$('.div_build_item').replaceWith(
+				'<div class="div_build_item" id="bld1" data-bind="visible: checkRestrictions($data.id()), event: { mouseover: function () { $parent.setBuildHover($index()) },' +
+				'                                                          mousedown: function (data,event) { $parent.executeStartBuild(event, $index())} }">' +
+				'    <span class="span_build_count" data-bind="text: count, visible: count() > 0"></span>' +
+				'    <a href="#" data-bind="rollover_sound_exclusive: { sound: \'default\', group: $index()}">' +
+				'        <img class="img_build_unit" src="img/build_bar/units/build_unit_sample.png" data-bind="attr: { src: icon }" />' +
+				'    </a>'+
+				'</div>'
 
 );
-//}
